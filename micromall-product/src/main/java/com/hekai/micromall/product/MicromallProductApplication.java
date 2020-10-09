17113a8ab25b211e39cf42deea45e20bbbd3a48c
HeziK <1584825075@qq.com>
2020-10-09 22:40:25 +0800

    部分更新

--- micromall-product/src/main/java/com/hekai/micromall/product/MicromallProductApplication.java
+++ micromall-product/src/main/java/com/hekai/micromall/product/MicromallProductApplication.java
@@ -4,12 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @MapperScan("com.hekai.micromall.product.dao")
 @SpringBootApplication
 @EnableDiscoveryClient
-@RefreshScope
 public class MicromallProductApplication {
 
     public static void main(String[] args) {
